d332e7d91aad823f67ecdd3b2d70e5be24ce5e4f
ThotaThanish <sriurr@gmail.com>
2017-04-04 22:35:59 +0530

    Update PrimeExample.java

--- PrimeExample.java
+++ PrimeExample.java
@@ -1,7 +1,7 @@
 class PrimeExample{  
  public static void main(String args[]){  
   int i,m=0,flag=0;    
-  int n=17;//it is the number to be checked  
+  int n=17;//it is the number to be checked  // it indicates comment or its not part of in the program  bye
   m=n/2;    
   for(i=2;i<=m;i++){    
    if(n%i==0){    
